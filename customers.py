3f73170467fa639e9d9000750d25ca8b1a4a2cde
Hackbright Student <info@hackbrightacademy.com>
2016-04-21 16:34:27 -0700

    created customer dictionary with login info

--- customers.py
+++ customers.py
@@ -7,3 +7,46 @@ class Customer(object):
     # TODO: need to implement this
 
 
+    def __init__(self, first_name, last_name, email, pw):
+        """Initialize customer"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.pw = pw
+
+    def __repr__(self):
+        """Prints customer information"""
+        return "Customer: {} {}, email address: {}, password: {}".format(self.first_name, 
+                                                                        self.last_name, 
+                                                                        self.email, 
+                                                                        self.pw)
+
+
+def read_customers_from_file(filepath):
+    """Read customers from a text file"""
+
+    customers = open(filepath)
+
+    customer_database = {}
+
+    for customer in customers:
+        customer = customer.strip()
+        customer = customer.split("|")
+        first_name, last_name, email, pw = customer
+
+
+        customer_database[email] = Customer(first_name, last_name, email, pw)
+
+    return customer_database
+
+
+def get_by_email(email):
+    """Get a customer by email address"""
+
+    return customers[email]
+
+
+
+customers = read_customers_from_file('customers.txt')
+
+# print customers
